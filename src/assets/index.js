ba67a6a8d1d7428a24a95a69748d2ebaf03c094e
Rafiul Islam <71514888+rafiulislam89@users.noreply.github.com>
2024-10-11 04:48:36 +0600

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,31 @@
-import close from "./close.svg";
-import logo from "./logo.svg";
-import menu from "./menu.svg";
+import React from 'react';
+import close from '../assets/close.svg';
+import logo from '../assets/logo.svg';
+import menu from '../assets/menu.svg';
+import algorithms from '../assets/algorithms.png';
+import devnotes from '../assets/devnotes.png';
+import oscs from '../assets/oscs.png';
 
-import algorithms from "./algorithms.png";
-import devnotes from "./devnotes.png";
-import oscs from "./oscs.png";
+const YourComponent = () => {
+  return (
+    <div>
 
-export { algorithms, close, devnotes, logo, menu, oscs };
+      <a href="https://github.com/rafiulislam89" target="_blank" rel="noopener noreferrer">
+        <img src={devnotes} alt="Dev Notes" />
+      </a>
+
+      <a href="https://rafiulislam.rf.gd" target="_blank" rel="noopener noreferrer">
+        <img src={oscs} alt="OSCS" />
+      </a>
+
+      <a href="https://example.com/algorithms" target="_blank" rel="noopener noreferrer">
+        <img src={algorithms} alt="Algorithms" />
+      </a>
+
+
+    </div>
+  );
+};
+
+export default YourComponent;
 
