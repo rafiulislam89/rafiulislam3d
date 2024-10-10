58346b3fc2c1d4dc690daf7452c71200677338de
Rafiul Islam <71514888+rafiulislam89@users.noreply.github.com>
2024-10-11 03:59:07 +0600

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -35,9 +35,9 @@ const experiences = [
     ],
   },
   {
-    title: "Software Developer",
-    company_name: "Indie",
-    date: "2019 - 2023",
+    title: "Software Engineer",
+    company_name: "Chokrojan",
+    date: "2024 - Present",
     details: [
       "Developed and delivered custom interdisciplinary coding portfolio for clients including <span style='color: white;'>Nvidia, Hostinger, and Amazon</span>.",
       "<span style='color: white;'>Designed and developed innovative</span> AI applications and interactive websites.",
@@ -46,8 +46,8 @@ const experiences = [
   },
   {
     title: "Software Engineer",
-    company_name: "Prime 3",
-    date: "2018 - 2019",
+    company_name: "Games World League",
+    date: "2023 - 2023",
     details: [
       "Built custom enterprise applications for a <span style='color: white;'>Fortune 500 company</span> as a full-stack software engineer.",
       "Developed and maintained <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
@@ -56,8 +56,8 @@ const experiences = [
   },
   {
     title: "Computer Science",
-    company_name: "ODU",
-    date: "2015 - 2018",
+    company_name: "UIU",
+    date: "2020 - 2024",
     details: [
       "Built a <span style='color: white;'>computer science foundation</span> learning theory, computer architecture, and software engineering.",
       "Worked and interned at <span style='color: white;'>NASA and Norfolk Southern Railway</span> to gain practical experience in the field of data analysis.",
