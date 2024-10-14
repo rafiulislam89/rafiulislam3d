18774ce4e28b7ab7187576bcfa9fcd377c6cca47
Rafiul Islam <71514888+rafiulislam89@users.noreply.github.com>
2024-10-15 02:15:27 +0600

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -14,8 +14,8 @@ export const navLinks = [
     title: "Portfolio",
   },
   {
-    id: "experience",
-    title: "Education  and Experience",
+    id: "Education & experience",
+    title: "Education and Experience",
   },
   {
     id: "contact",
@@ -74,9 +74,9 @@ const portfolio = [
     image: oscs,
   },
   {
-    name: "Decent portfolio",
+    name: "Discover the Details",
     description:
-    "This is my decent portfolio. I built it using HTML, CSS, and JavaScript. To visit: http://rafiulislam.rf.gd",
+    "As salamualaikum, This is my another portfolio. where I’ve put together a collection of my projects, skills, and experiences built with HTML, CSS, and JavaScript. Here, you’ll find not only my work but also my CV, giving you a deeper understanding of who I am and what I love doing. If you’re curious to know more about me and see how I approach web development, take a moment to explore. To dive in, visit: http://rafiulislam.rf.gd.",
     image: devnotes,
     link: "http://rafiulislam.rf.gd",
   },
